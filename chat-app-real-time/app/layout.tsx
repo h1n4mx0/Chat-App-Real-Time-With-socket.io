6b7bf3722945c175279f813f6f00a96ca66dd3df
NguyenCongHaiNam <h1n4m.backhat@gmail.com>
2025-01-17 12:47:38 +0700

    update

--- chat-app-real-time/app/layout.tsx
+++ chat-app-real-time/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="mx-auto px-4">{children}</div>
+        
       </body>
     </html>
   );
